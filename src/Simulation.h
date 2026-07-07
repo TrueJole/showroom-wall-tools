c528d3e36836cfecc56f014eff18bc9f2010160e
TrueJole <jole.koop@gmail.com>
2026-07-07 11:17:44 +0200

    init test

---
+++ src/Simulation.h
@@ -0,0 +1,82 @@
+#include <Arduino.h>
+#include <HAMqtt.h>
+#include <string>
+
+const char* EVENT_NAMES[] = {"winter", "shower", "summer"};
+
+enum events {
+	SIM_WINTER,
+	SIM_SHOWER,
+	SIM_SUMMER,
+	EVENTS_LENGTH
+};
+
+class Simulation {
+	
+public:
+	Simulation(HAMqtt *mqtt_) {
+		mqtt = mqtt_;
+	}
+
+	bool checkStatus(int event) {
+		return eventStatus[event];
+	}
+
+	int checkDuration(int event) {
+		return millis() - eventStart[event];
+	}
+
+	void simulationSetup() {
+		for (int i = 0; i < EVENTS_LENGTH; i++) {
+			//Serial.println(strcat(strdup("simulated/"), strdup(EVENT_NAMES[i])));
+			std::string topic = std::string("simulated/") + EVENT_NAMES[i];
+			mqtt->subscribe(topic.c_str());
+		}
+		Serial.println("Simulation - Setup complete");
+		//mqtt->subscribe("simulated/winter");
+	}
+
+	void handleSimulation(const char *topic, const uint8_t *payload, uint16_t length) {
+		Serial.print("Simulation - Got message ");
+		Serial.print(topic);
+		Serial.print(" : ");
+
+		char payload_data[256];
+		strncpy(payload_data, (const char *)payload, length);
+		payload_data[length] = '\0';
+
+		Serial.println((const char *)payload_data);
+
+		for (int i = 0; i < EVENTS_LENGTH; i++) {
+			//Serial.print("Simulation - Comparing to ");
+			//Serial.println(strcat(strdup("simulated/"), strdup(EVENT_NAMES[i])));
+			std::string event = std::string("simulated/") + EVENT_NAMES[i];
+			if (strcmp(topic, event.c_str()) == 0) {
+				//Serial.println("Found topic!");
+				if (strcmp(payload_data, "ON") == 0) {
+					//Serial.print("Simulation - ");
+					//Serial.print(EVENT_NAMES[i]);
+					//Serial.println(" on");
+					//Serial.print("Storing in element ");
+					//Serial.println(i);
+					eventStatus[i] = true;
+					eventStart[i] = millis();
+				} else {
+					//Serial.print("Simulation - ");
+					//Serial.print(EVENT_NAMES[i]);
+					//Serial.println(" off");
+					eventStatus[i] = false;
+				}
+				Serial.println("Simulation - Message handled!");
+				return;
+			}
+		}
+		Serial.println("Simulation - Message unhandled!");
+	}
+
+private:
+	bool eventStatus[EVENTS_LENGTH];
+	unsigned long eventStart[EVENTS_LENGTH];
+	HAMqtt* mqtt;
+};
+
