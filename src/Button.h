bf2a5f18171617605d0fa3ec7999f9c86f2d3a38
TrueJole <jole.koop@gmail.com>
2026-07-09 09:38:40 +0200

    initial button support

---
+++ src/Button.h
@@ -0,0 +1,93 @@
+#include <Arduino.h>
+#include <Lamp.h>
+
+class Button {
+  public:
+    void registerSwitchLamp(Lamp *lamp) {
+      switchLamp = lamp;
+    }
+
+    bool getState() {
+      return isPressed;
+    }
+
+    bool handleRelease() {
+      if (releaseReady) {
+        releaseReady = false;
+        return true;
+      }
+      return false;
+    }
+
+    void manualPress(unsigned long duration, bool force) {
+      bool previousState = isPressed;
+
+      // Ignore if already pressed and not forcing
+      if (previousState && !force) {
+        return;
+      }
+
+      isPressed = true;
+      if (onPressCallback != nullptr) {
+        onPressCallback();
+      }
+      
+      delay(duration);
+
+      isPressed = previousState;
+      if (onReleaseCallback != nullptr) {
+        onReleaseCallback();
+      }
+      if (switchLamp != nullptr) switchLamp->turnSwitch();
+
+      releaseReady = true;
+    }
+
+    void loop() {
+      bool state = digitalRead(pin);
+      if (digitalRead(pin) != pullUp) {
+        isPressed = true;
+        if (!lastState) {
+          if (onPressCallback != nullptr) {
+            onPressCallback();
+          }
+        }
+      }
+      else {
+        isPressed = false;
+        if (lastState) {
+          releaseReady = true;
+          if (onReleaseCallback != nullptr) {
+            onReleaseCallback();
+          }
+          if (switchLamp != nullptr) switchLamp->turnSwitch();
+        }
+      }
+      lastState = isPressed;
+    }
+
+    void onPress(void(*onPress)()) {
+      onPressCallback = onPress;
+    }
+
+    void onRelease(void(*onRelease)()) {
+      onReleaseCallback = onRelease;
+    }
+
+    Button(int pin_, bool pullUp_) {
+      pin = pin_;
+      pullUp = pullUp_;
+      
+      pinMode(pin, INPUT);
+    }
+
+  private:
+    int pin;
+    bool pullUp;
+    void(*onPressCallback)();
+    void(*onReleaseCallback)();
+    Lamp *switchLamp;
+    bool isPressed = false;
+    bool lastState = false;
+    bool releaseReady = false;
+};
