7cec2442439d4c4edb7d898cf1da7109523823e5
TrueJole <jole.koop@gmail.com>
2026-07-09 13:40:51 +0200

    Add better button integration class to HA

---
+++ src/Button2.h
@@ -0,0 +1,67 @@
+#include <ArduinoHA.h> 
+#include <unordered_map>
+#include <Arduino.h>
+
+// DO NOT USE BUTTON AND BUTTON2 TOGETHER
+#include <JC_Button_ESP.h>
+
+class Button2 {
+  public:
+    bool isPressed() {
+      return pressed;
+    }
+
+    void manualPress(uint32_t duration) {
+      Serial.print("Button - Manual press for ");
+      Serial.print(duration);
+      Serial.println(" milliseconds!");
+
+      actor.setState(true);
+      pressReady = true;
+      pressed = true;
+      delay(duration);
+
+      actor.setState(false);
+      pressed = false;
+    } 
+
+    void loop() {
+      JCbutton.read();
+      
+      if (JCbutton.wasPressed()) {
+        pressReady = true;
+      }
+
+      if (JCbutton.isPressed()) {
+        actor.setState(true);
+        pressed = true;
+      }
+      else {
+        actor.setState(false);
+        pressed = false;
+      }
+    }
+
+    bool handlePress() {
+      if (pressReady) {
+        pressReady = false;
+        return true;
+      }
+      return false;
+    }
+
+    Button2(const char* name, const char* id, int pin, bool pullUp)
+      : actor(id),
+        JCbutton(pin, 25UL, 1U, pullUp)
+    {
+      actor.setName(name);
+      actor.setDeviceClass("plug");
+      JCbutton.begin();
+    }
+
+  private:
+    HABinarySensor actor;
+    Button JCbutton;
+    bool pressReady = false;
+    bool pressed = false;
+};
